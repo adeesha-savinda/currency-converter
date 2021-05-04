731d7246486214b6add409ee09e7106de829abc3
adeesha-savinda <adeesha.savinda@gmail.com>
2021-05-04 13:40:24 +0530

    removed debugging console logs

--- middlewares/cacheMiddleware.js
+++ middlewares/cacheMiddleware.js
@@ -5,8 +5,6 @@ const cacheMiddleware = (req, res, next) => {
             next(err);
         }
         //if values are available in the cache
-        console.log(value)
-        console.log(req.body)
         if (value) {
             if (value.from_currency === fromCurrency && value.to_currency === toCurrency) {
                 return res.status(200).json({
